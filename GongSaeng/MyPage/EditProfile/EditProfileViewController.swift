fad193bed1c86be68672d0ce0f6722d51323d7d6
DongCheon Jeong <oluye7@naver.com>
2021-09-21 00:12:19 +0900

    마이페이지 - 프로필 수정  UI 수정

--- GongSaeng/MyPage/EditProfile/EditProfileViewController.swift
+++ GongSaeng/MyPage/EditProfile/EditProfileViewController.swift
@@ -13,21 +13,73 @@ class EditProfileViewController: UIViewController {
     @IBOutlet weak var affiliationTextField: UITextField!
     @IBOutlet weak var websiteTextField: UITextField!
     @IBOutlet weak var introductionTextView: UITextView!
-    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var completeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         userImageView.roundCornerOfImageView()
         
-        [nickNameTextField, affiliationTextField, websiteTextField].forEach { $0.underlined(viewSize: view.bounds.width, color: UIColor.systemGray) }
+        [nickNameTextField, affiliationTextField, websiteTextField].forEach { $0.underlined(viewSize: view.bounds.width, color: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.2)) }
         
-        // introductionTextView의 밑줄 구현하기
+        let size = CGSize(width: introductionTextView.frame.width, height: .infinity)
+        let estimatedSize = introductionTextView.sizeThatFits(size)
+        
+        introductionTextView.constraints.forEach { (constraint) in
+            if constraint.firstAttribute == .height {
+                constraint.constant = estimatedSize.height
+            }
+        }
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardHideShow(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+    
+    
+// MARK:- 키보드 보일 때 함수
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        if let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            let scrollContentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardFrame.size.height, right: 0)
+            scrollView.contentInset = scrollContentInset
+            scrollView.contentOffset = CGPoint(x: 0, y: keyboardFrame.size.height)
+        }
+    }
+// MARK:- 키보드 내릴 때 함수
+    @objc func keyboardHideShow(_ notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        let animationDuration = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+
+        UIView.animate(withDuration: animationDuration, animations: {
+            self.view.layoutIfNeeded()
+        })
+
+        let scrollContentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        scrollView.contentInset = scrollContentInset
+        scrollView.contentOffset = CGPoint(x: 0, y: 0)
+    }
+    
+    
+    
     
     @IBAction func backwardButtonTapped(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
-    
-    
+}
+
+
+// MARK:- 텍스트뷰 델리게이트
+extension EditProfileViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        let size = CGSize(width: textView.frame.width, height: .infinity)
+        let estimatedSize = textView.sizeThatFits(size)
+        
+        textView.constraints.forEach { (constraint) in
+            if constraint.firstAttribute == .height {
+                constraint.constant = estimatedSize.height
+            }
+        }
+    }
 }
