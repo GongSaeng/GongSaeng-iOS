1fcc6885dc13633da79087e86e60aeccb2bb18e7
benny1020 <benny1020@naver.com>
2021-12-29 16:54:44 +0900

    자유게시판 쓰기 밑 읽어오기

--- GongSaeng/Community/free/freeDetailViewController.swift
+++ GongSaeng/Community/free/freeDetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  NoticeDetailViewController.swift
+//  freeDetailViewController.swift
 //  GongSaeng
 //
 //  Created by 정동천 on 2021/10/26.
@@ -7,20 +7,22 @@
 
 import UIKit
 
-class NoticeDetailViewController: UIViewController {
+class freeDetailViewController: UIViewController {
     
     // MARK: Properties
-    var notice: Notice?
+    var free: free?
     
-    private lazy var commentInputView: CommentInputAccesoryView = {
+    private lazy var freeCommentInputView: CommentInputAccesoryView = {
         let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 150.0)
-        let commentInputAccesoryView = CommentInputAccesoryView(frame: frame)
-        return commentInputAccesoryView
+        let CommentInputAccesoryView = CommentInputAccesoryView(frame: frame)
+        return CommentInputAccesoryView
     }()
     
     @IBOutlet weak var postingUserImageView: UIImageView!
     
-    @IBOutlet weak var categoryLabel: UILabel!
+   //
+    
+   // @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var titleLabel: UITextView!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var contentsLabel: UITextView!
@@ -39,7 +41,7 @@ class NoticeDetailViewController: UIViewController {
     }
     
     override var inputAccessoryView: UIView? {
-        get { return commentInputView }
+        get { return freeCommentInputView }
     }
     
     override var canBecomeFirstResponder: Bool {
@@ -60,17 +62,17 @@ class NoticeDetailViewController: UIViewController {
         
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
-        if let notice = notice {
-            categoryLabel.text = notice.category
-            titleLabel.text = notice.title
-            timeLabel.text = notice.time
-            contentsLabel.text = notice.contents
+        if let free = free {
+            //categoryLabel.text = free.category
+            titleLabel.text = free.title
+            timeLabel.text = free.time
+            contentsLabel.text = free.contents
         }
     }
     
     private func configureNavigationView() {
         navigationController?.navigationBar.isHidden = false
-        navigationItem.title = "공지사항"
+        navigationItem.title = "자유게시판"
         navigationController?.navigationBar.tintColor = UIColor(named: "colorPaleOrange")
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18.0, weight: .medium)]
         
@@ -85,27 +87,36 @@ class NoticeDetailViewController: UIViewController {
     }
 }
 
-class NoticeTableView: UITableView {
+class freeTableView: UITableView {
     
 }
 
-
-class CommentTableViewCell: UITableViewCell {
-    @IBOutlet weak var commentWriterImageView: UIImageView!
-    @IBOutlet weak var commentWriterNicknameLabel: UILabel!
-    @IBOutlet weak var commentedTimeLabel: UILabel!
-    @IBOutlet weak var commentLabel: UILabel!
+class ImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var attachedImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        commentWriterImageView.layer.cornerRadius = commentWriterImageView.frame.height / 2
-        commentWriterImageView.layer.borderWidth = 1
-        commentWriterImageView.layer.borderColor = UIColor(white: 0.0, alpha: 0.1).cgColor
+        attachedImageView.layer.cornerRadius = 8
     }
 }
 
-extension NoticeDetailViewController: UITextViewDelegate {
+class freeCommentTableViewCell: UITableViewCell {
+    @IBOutlet weak var CommentWriterImageView: UIImageView!
+    @IBOutlet weak var CommentWriterNicknameLabel: UILabel!
+    @IBOutlet weak var CommentedTimeLabel: UILabel!
+    @IBOutlet weak var CommentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        CommentWriterImageView.layer.cornerRadius = CommentWriterImageView.frame.height / 2
+        CommentWriterImageView.layer.borderWidth = 1
+        CommentWriterImageView.layer.borderColor = UIColor(white: 0.0, alpha: 0.1).cgColor
+    }
+}
+
+extension freeDetailViewController: UITextViewDelegate {
     func textViewDidBeginEditing(_ textView: UITextView) {
         let placeHolderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.5)
         if textView.textColor == placeHolderColor {
@@ -129,7 +140,7 @@ extension NoticeDetailViewController: UITextViewDelegate {
     }
 }
 
-extension NoticeTableView: UICollectionViewDataSource {
+extension freeTableView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
@@ -140,27 +151,27 @@ extension NoticeTableView: UICollectionViewDataSource {
     }
 }
 
-extension NoticeTableView: UICollectionViewDelegate {
+extension freeTableView: UICollectionViewDelegate {
     
 }
 
-extension NoticeTableView: UICollectionViewDelegateFlowLayout {
+extension freeTableView: UICollectionViewDelegateFlowLayout {
     
 }
 
-extension NoticeDetailViewController: UITableViewDataSource {
+extension freeDetailViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CommentTableViewCell", for: indexPath) as? CommentTableViewCell else { return CommentTableViewCell() }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "freeCommentTableViewCell", for: indexPath) as? freeCommentTableViewCell else { return freeCommentTableViewCell() }
         
         return cell
     }
 }
 
-extension NoticeDetailViewController: UITableViewDelegate {
+extension freeDetailViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         100
     }
