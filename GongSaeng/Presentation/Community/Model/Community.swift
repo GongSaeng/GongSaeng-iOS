6156566e1c77c8ad86b4dcaf63a264ab941f72d0
cyj893 <65068871+cyj893@users.noreply.github.com>
2022-09-28 21:08:32 +0900

    Feature/community (#5)
    * fix: Remove `page` in query
    
    현재 서버에 페이지네이션을 구현하지 않으므로, 삭제합니다.
    
    * feat: Move queries to body param
    
    POST이고, 바디에 해당 파라미터들을 넣어야 하므로 수정합니다.
    
    * feat: Add `Community2`
    
    서버에서 다양한 방식으로 데이터가 주어지므로, category와 price 등이 없는 경우의 모델을 추가합니다.
    추후 게시글 성격마다 달라지는 모델들을 추가해야 하며, 이름을 리팩토링해야 합니다.
    
    * feat: 다양한 형식의 모델 지원
    
    서버에서 주어지는 형식이 정해져있지 않으므로, 여러 모델로 디코딩을 시도합니다.
    
    * feat: Use `dedcodeIfPresent`
    
    게시판마다 달라지기 때문에 해당 키가 있는 경우에만 넣어주는 것으로 수정합니다.
    
    * remove: Remove `Community2`

--- GongSaeng/Presentation/Community/Model/Community.swift
+++ GongSaeng/Presentation/Community/Model/Community.swift
@@ -25,11 +25,27 @@ struct Community: Decodable {
         case index = "idx"
         case title, contents, category, price
         case writerId = "id"
-        case writerNickname = "nickname"
+        case writerNickname = "name"
         case numberOfComments = "comment_cnt"
         case uploadedTime = "time"
         case validStatus = "status"
         case thumbnailImageFilename = "image_url"
         case writerImageFilename = "writer_profile_image"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.index = try container.decode(Int.self, forKey: .index)
+        self.validStatus = try container.decodeIfPresent(Int.self, forKey: .validStatus)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.contents = try container.decode(String.self, forKey: .contents)
+        self.writerImageFilename = try container.decodeIfPresent(String.self, forKey: .writerImageFilename)
+        self.writerId = try container.decode(String.self, forKey: .writerId)
+        self.writerNickname = try container.decode(String.self, forKey: .writerNickname)
+        self.uploadedTime = try container.decode(String.self, forKey: .uploadedTime)
+        self.numberOfComments = try container.decode(Int.self, forKey: .numberOfComments)
+        self.thumbnailImageFilename = try container.decodeIfPresent(String.self, forKey: .thumbnailImageFilename)
+        self.category = try container.decodeIfPresent(String.self, forKey: .category)
+        self.price = try container.decodeIfPresent(String.self, forKey: .price)
     }
 }
