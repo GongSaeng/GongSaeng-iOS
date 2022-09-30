1a4be26191498cb3a3e10cb1ea99ab3041d7423d
cyj893 <65068871+cyj893@users.noreply.github.com>
2022-09-30 13:20:22 +0900

    Feature/post (#9)
    * feat: Make `Post` can decode dynamic
    
    `Community`와 마찬가지로, 다이나믹하게 파라미터들에 대응하여 디코드할 수 있도록 합니다.
    
    * feat: Separate checking response and decoding
    
    리스폰스와 디코딩 부를 나누어서 에러를 명확히 합니다.

--- GongSaeng/Presentation/Community/Model/Post.swift
+++ GongSaeng/Presentation/Community/Model/Post.swift
@@ -28,10 +28,44 @@ struct Post: Decodable {
         case uploadedTime = "time"
         case postingImagesFilename = "contents_images_url"
         case writerImageFilename = "profile_image_irl"
-        case status, category, price
+        case status = "status"
+        case gather_status
+        case category, price
     }
     
     mutating func updateNumberOfComments(numberOfComments: Int) {
         self.numberOfComments = numberOfComments
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        if let decodedStatus = try container.decodeIfPresent(Int.self, forKey: .status) {
+            self.status = decodedStatus
+        } else if let decodedStatus = try container.decodeIfPresent(Int.self, forKey: .gather_status) {
+            self.status = decodedStatus
+        }
+        
+        self.title = try container.decode(String.self, forKey: .title)
+        self.contents = try container.decode(String.self, forKey: .contents)
+        self.writerImageFilename = try container.decodeIfPresent(String.self, forKey: .writerImageFilename)
+        self.writerId = try container.decode(String.self, forKey: .writerId)
+        self.writerNickname = try container.decode(String.self, forKey: .writerNickname)
+        self.uploadedTime = try container.decode(String.self, forKey: .uploadedTime)
+        self.numberOfComments = try container.decode(Int.self, forKey: .numberOfComments)
+        
+        do {
+            if let decodedFileName = try container.decodeIfPresent(String.self, forKey: .postingImagesFilename) {
+                self.postingImagesFilename = decodedFileName.components(separatedBy: ",")
+            }
+        } catch {
+            self.postingImagesFilename = try container.decodeIfPresent([String].self, forKey: .postingImagesFilename)
+        }
+        
+        self.category = try container.decodeIfPresent(String.self, forKey: .category)
+        
+        if let decodedPrice = try container.decodeIfPresent(Int.self, forKey: .price) {
+            self.price = String(decodedPrice)
+        }
     }
 }
