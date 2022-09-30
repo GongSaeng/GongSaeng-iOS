462428a66c3d0a34367792285953ba88992fb0c8
cyj893 <65068871+cyj893@users.noreply.github.com>
2022-09-30 19:52:41 +0900

    Feature/create post (#12)
    * feat: Split errors
    
    어디서 에러가 나는 지 명확히 하기 위해, 디코딩 부분을 따로 빼내고 데이터를 출력합니다.
    
    * feat: Apply right form for api
    
    카테고리와 가격이 있는 경우도 추가해서 넣어 줍니다.
    이미지가 있을 때와 없을 때를 구분할 필요가 없으므로 해당 분기를 삭제합니다.

--- GongSaeng/Network/CommunityNetworkManager.swift
+++ GongSaeng/Network/CommunityNetworkManager.swift
@@ -25,7 +25,7 @@ final class CommunityNetworkManager {
             }
             
             guard let communitys = try? JSONDecoder().decode([Community].self, from: data) else {
-                print("ERROR: Community 변환 실패")
+                print("ERROR: Community 변환 실패:", String(data: data, encoding: .utf8) ?? "")
                 return
             }
             
@@ -74,7 +74,7 @@ final class CommunityNetworkManager {
             }
             
             guard let post = try? JSONDecoder().decode([Post].self, from: data).first else {
-                print("Error: Decoding post")
+                print("Error: Decoding post ->", String(data: data, encoding: .utf8) ?? "")
                 return
             }
             
@@ -114,20 +114,32 @@ final class CommunityNetworkManager {
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
+        
+        let boundary = "Boundary-\(UUID().uuidString)"
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        var data = Data()
+        
+        let boundaryPrefix = "--\(boundary)\r\n"
+        let boundarySuffix = "--\(boundary)--\r\n"
+        
+        var params: [(String, Any)] = [("code", code), ("title", title), ("contents", contents), ("time", time)]
+        
+        if let category = category {
+            params.append(("category", category))
+            print("catette", category)
+        }
+        if let price = price {
+            params.append(("price", price))
+        }
+        
+        params.forEach { (key, value) in
+            data.append(boundaryPrefix.data(using: .utf8)!)
+            data.append("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".data(using: .utf8)!)
+            data.append("\(value)\r\n".data(using: .utf8)!)
+        }
+        
         if let images = images, let thumbnailImage = images.first {
-            let boundary = "Boundary-\(UUID().uuidString)"
-            request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-            
-            var data = Data()
-            
-            let boundaryPrefix = "--\(boundary)\r\n"
-            let boundarySuffix = "--\(boundary)--\r\n"
-            ["code": code, "title": title, "contents": contents, "time": time].forEach { (key, value) in
-                data.append(boundaryPrefix.data(using: .utf8)!)
-                data.append("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".data(using: .utf8)!)
-                data.append("\(value)\r\n".data(using: .utf8)!)
-            }
-            
             for image in images {
                 guard let imageData = image.jpegData(compressionQuality: 0.5) else { return }
 
@@ -140,76 +152,41 @@ final class CommunityNetworkManager {
                 data.append(imageData)
                 data.append("\r\n".data(using: .utf8)!)
             }
-
-            data.append(boundarySuffix.data(using: .utf8)!)
-            
-            let dataTask = URLSession.shared.uploadTask(with: request, from: data) { data, response, error in
-                guard error == nil,
-                      let response = response as? HTTPURLResponse,
-                      let data = data,
-                      let returnValue = String(data: data, encoding: .utf8)  else {
-                    print("ERROR: URLSession data task \(error?.localizedDescription ?? "")")
-                    return
-                }
-                switch response.statusCode {
-                case (200...299):
-                    print("DEBUG: postCommunity response is succeded..", returnValue)
-                    let isSucceded = (returnValue == "true") ? true : false
-                    completion(isSucceded)
-                case (400...499):
-                    print("""
-                        ERROR: Client ERROR \(response.statusCode)
-                        Response: \(response)
-                    """)
-                case (500...599):
-                    print("""
-                        ERROR: Server ERROR \(response.statusCode)
-                        Response: \(response)
-                    """)
-                default:
-                    print("""
-                        ERROR: \(response.statusCode)
-                        Response: \(response)
-                    """)
-                }
-            }
-            dataTask.resume()
-             
-        } else {
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-            let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
-                guard error == nil,
-                      let response = response as? HTTPURLResponse,
-                      let data = data,
-                      let returnValue = String(data: data, encoding: .utf8)  else {
-                          print("ERROR: URLSession data task \(error?.localizedDescription ?? "")")
-                          return
-                      }
-
-                switch response.statusCode {
-                case (200...299):
-                    let isSucceded = (returnValue == "true") ? true : false
-                    completion(isSucceded)
-                case (400...499):
-                    print("""
-                        ERROR: Client ERROR \(response.statusCode)
-                        Response: \(response)
-                    """)
-                case (500...599):
-                    print("""
-                        ERROR: Server ERROR \(response.statusCode)
-                        Response: \(response)
-                    """)
-                default:
-                    print("""
-                        ERROR: \(response.statusCode)
-                        Response: \(response)
-                    """)
-                }
-            }
-            dataTask.resume()
         }
+        
+        data.append(boundarySuffix.data(using: .utf8)!)
+        
+        let dataTask = URLSession.shared.uploadTask(with: request, from: data) { data, response, error in
+            guard error == nil,
+                  let response = response as? HTTPURLResponse,
+                  let data = data,
+                  let returnValue = String(data: data, encoding: .utf8)  else {
+                print("ERROR: URLSession data task \(error?.localizedDescription ?? "")")
+                return
+            }
+            switch response.statusCode {
+            case (200...299):
+                print("DEBUG: postCommunity response is succeded..", returnValue)
+                let isSucceded = (returnValue == "true") ? true : false
+                completion(isSucceded)
+            case (400...499):
+                print("""
+                    ERROR: Client ERROR \(response.statusCode)
+                    Response: \(response)
+                """)
+            case (500...599):
+                print("""
+                    ERROR: Server ERROR \(response.statusCode)
+                    Response: \(response)
+                """)
+            default:
+                print("""
+                    ERROR: \(response.statusCode)
+                    Response: \(response)
+                """)
+            }
+        }
+        dataTask.resume()
     }
     
     static func fetchMyPosts(myPostType: MyPostType, completion: @escaping([MyPost]) -> Void) {
@@ -266,11 +243,16 @@ final class CommunityNetworkManager {
         let dataTask = URLSession.shared.dataTask(with: request) {data, response, error in
             guard error == nil,
                   let response = response as? HTTPURLResponse,
-                  let data = data,
-                  let comments = try? JSONDecoder().decode([Comment].self, from: data) else {
-                      print("ERROR: URLSession data task \(error?.localizedDescription ?? "")")
-                      return
-                  }
+                  let data = data else {
+                print("ERROR: URLSession data task \(error?.localizedDescription ?? "")")
+                return
+            }
+            
+            guard let comments = try? JSONDecoder().decode([Comment].self, from: data) else {
+                print("ERROR: comments decoding failed ->", String(data: data, encoding: .utf8) ?? "")
+                return
+            }
+            
             print("DEBUG: comments index ->", comments)
             
             switch response.statusCode {
