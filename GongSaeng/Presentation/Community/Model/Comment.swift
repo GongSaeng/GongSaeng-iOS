494aa46ea0063f042b4544162c2ec2c5b3bb2d60
cyj893 <cyj89317@gmail.com>
2022-10-06 20:16:56 +0900

    feat: Add more CodingKeys and decode self
    번개, 커뮤니티에 따라 파라미터 이름이 다르게 오므로 코딩키를 추가하고 직접 디코딩합니다.

--- GongSaeng/Presentation/Community/Model/Comment.swift
+++ GongSaeng/Presentation/Community/Model/Comment.swift
@@ -10,13 +10,35 @@ import Foundation
 struct Comment: Decodable {
     var contents: String
     var writerImageFilename: String?
-    var writerNickname: String
-    var uploadedTime: String
+    var writerNickname: String = ""
+    var uploadedTime: String = ""
     
     enum CodingKeys: String, CodingKey {
         case contents
         case writerNickname = "nickname"
+        case m_nickname
         case uploadedTime = "time"
+        case regdate
         case writerImageFilename = "profile_image_url"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        if let decodedNickname = try container.decodeIfPresent(String.self, forKey: .writerNickname) {
+            self.writerNickname = decodedNickname
+        } else if let decodedNickname = try container.decodeIfPresent(String.self, forKey: .m_nickname) {
+            self.writerNickname = decodedNickname
+        }
+        
+        if let decodedTime = try container.decodeIfPresent(String.self, forKey: .uploadedTime) {
+            self.uploadedTime = decodedTime.convertEnToKo()
+        } else if let decodedTime = try container.decodeIfPresent(String.self, forKey: .regdate) {
+            self.uploadedTime = decodedTime.convertEnToKo()
+        }
+        
+        self.contents = try container.decode(String.self, forKey: .contents)
+        self.writerImageFilename = try container.decodeIfPresent(String.self, forKey: .writerImageFilename)
+        
     }
 }
