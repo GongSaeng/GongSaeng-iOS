ff81058bee1b61cec4300535b47bd533f2d36e2d
cyj893 <cyj89317@gmail.com>
2022-10-06 21:17:59 +0900

    remove: Remove `MyThunder` and Add `MyThunderData`
    기존 MyThunder는 필요가 없으므로 대신 ThunderDetailInfo를 사용합니다.

--- GongSaeng/Presentation/Thunder/Model/MyThunder.swift
+++ GongSaeng/Presentation/Thunder/Model/MyThunder.swift
@@ -7,22 +7,6 @@
 
 import Foundation
 
-struct MyThunder: Decodable {
-    var postIndex: Int
-    var postingImageFilename: String
-    var title: String
-    
-    var meetingTime: String
-    var placeName: String
-    var address: String
-    var placeURL: String
-    var totalNum: Int
-    
-    var contents: String
-    
-    var participantsProfile: [Profile]
-    
-    var status: Int
-    
-    var numberOfComments: Int
+struct MyThunderData: Decodable {
+    var data: [ThunderDetailInfo]
 }
