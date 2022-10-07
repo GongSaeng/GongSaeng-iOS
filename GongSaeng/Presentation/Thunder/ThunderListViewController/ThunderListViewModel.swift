37e6bcc2be7c37d926dc1a327bd814dfca354dc0
cyj893 <cyj89317@gmail.com>
2022-10-07 15:18:05 +0900

    feat: Reload myThunders when button is tapped
    기존에는 사용자가 어떠한 번개에 참여한 후 다시 내 번개 보기를 눌러도, 이전 데이터만 계속 사용하는 문제가 있습니다.
    따라서 버튼을 누를 때마다 리로드합니다.
    필요없는 변수는 삭제합니다.

--- GongSaeng/Presentation/Thunder/ThunderListViewController/ThunderListViewModel.swift
+++ GongSaeng/Presentation/Thunder/ThunderListViewController/ThunderListViewModel.swift
@@ -18,8 +18,6 @@ enum SortingOrder: String {
 final class ThunderListViewModel {
     private let disposeBag = DisposeBag()
     
-    let myThunders = BehaviorSubject<[ThunderDetailInfo]?>(value: nil)
-    
     // Subview's ViewModel
     let thunderListTopViewModel = ThunderListTopViewModel()
     let thunderListTableViewModel = ThunderListTableViewModel()
@@ -57,16 +55,6 @@ final class ThunderListViewModel {
             .bind(to: thunderListTableViewModel.thunderCellData)
             .disposed(by: disposeBag)
 
-        let myThundersResult = model.fetchMyThunders()
-        let myThundersValue = myThundersResult
-            .map(model.getMyThundersValue)
-            .asObservable()
-        
-        myThundersValue
-            .bind(to: myThunders)
-            .disposed(by: disposeBag)
-            
-        
         // UserDefaults(Singletone) -> ViewModel
         UserDefaults.standard.rx
             .observe(String.self, "region")
@@ -106,7 +94,10 @@ final class ThunderListViewModel {
         self.pushThunderView = thunderListTableViewModel.selectedIndex
         
         self.pushMyThunderView = myThunderButtonTapped
-            .withLatestFrom(myThunders)
+            .flatMap {
+                model.fetchMyThunders()
+                    .map(model.getMyThundersValue)
+            }
             .compactMap { $0 }
             .asSignal(onErrorJustReturn: [])
     }
